56689f2804e096ee4935417313ff55203510f924
Prashant Chakraborty <prashantchakraborty@Prashants-MBP.lan>
2023-12-10 17:18:08 -0600

    episode-01

---
+++ App.js
@@ -0,0 +1,22 @@
+
+const parent = React.createElement("div", {id: "parent"}, [
+    
+    React.createElement("div", {id: "child"}, [
+    React.createElement("h1", {}, "I am an h1"), 
+    React.createElement("h2", {}, "I am an h2")]),
+
+    React.createElement("div", {id: "child2"}, [
+    React.createElement("h1", {}, "I am an h1"), 
+    React.createElement("h2", {}, "I am an h2")])
+
+]);
+
+
+
+const heading = React.createElement("h1", {id: 'heading-1'}, "Hi from React!");
+
+console.log(parent);
+
+const myRoot = ReactDOM.createRoot(document.querySelector("#root"));
+
+myRoot.render(parent);

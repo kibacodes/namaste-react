bf6d74a29dc0ab8d5d7e4181138ca2f9fec73f22
Prashant Chakraborty <prashantchakraborty@Prashants-MBP.lan>
2023-12-19 20:53:27 -0600

    Namaste React episode 3 part 3

--- App.js
+++ App.js
@@ -5,8 +5,14 @@ import ReactDOM from 'react-dom/client';
 
 // React.createElement => Object => HTMLElement(render)
 
-const heading = React.createElement('h1', {id: 'heading'}, "Namaste React 🚀" )
+// const heading = React.createElement('h1', {id: heading}, 'Namaste React 🚀'); 
+// using React
+// console.log(heading);
+
+// JSX - HTML-like or XML-like syntax
+const jsxHeading = <h1 id="heading"> Namaste React using JSX 🚀</h1>; // using JSX
+console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(heading);
+root.render(jsxHeading);

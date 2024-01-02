d332e70c88d36edd9459903d207e235ee8ba77a5
Prashant Chakraborty <prashantchakraborty@Prashants-MBP.lan>
2024-01-01 22:38:52 -0600

    Namaste React Episode 3 Part 5

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const Title = () => (
+const title = (
     <h1 className="head" tabIndex="5">
         Namaste React using JSX 🚀
     </h1>
 )
 
+const number = 100;
 const HeadingComponent = () => (
     <div id="container">
-        <Title />
+        {title}
+        <h2>{number + 200}</h2>
+        {console.log('JS is here')}
         <h1 className="heading">Namaste React Functional Component</h1>
     </div>
 );

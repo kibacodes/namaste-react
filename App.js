e94dc72020af29b6fccdfd06c7e08ddebcd3da79
Prashant Chakraborty <prashantchakraborty@Prashants-MBP.lan>
2024-01-01 23:16:42 -0600

    Namaste React Episode 3 Practice

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const elem = <span>React Element</span>
+const Title = () => (
+    <h1 className="head" tabIndex="5">
+        Namaste React using JSX 🚀
+    </h1>
+);
+
 
 const HeadingComponent = () => (
     <div id="container">
-        {title}
+        <Title />
         <h1 className="heading">Namaste React Functional Component</h1>
     </div>
 );
 
-const title = (
-    <h1 className="head" tabIndex="5">
-        {elem}
-        Namaste React using JSX 🚀
-    </h1>
-);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 

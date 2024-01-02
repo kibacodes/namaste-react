d0bcbd13438313dc22f5e2273d55548571f46902
Prashant Chakraborty <prashantchakraborty@Prashants-MBP.lan>
2024-01-01 22:08:27 -0600

    Namaste React Ep 3 Pt 4- Functional Components

--- App.js
+++ App.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 // React Element
-
-// React.createElement => Object => HTMLElement(render)
-
-// const heading = React.createElement('h1', {id: heading}, 'Namaste React 🚀'); 
-// using React
-// console.log(heading);
-
-// JSX - HTML-like or XML-like syntax
 const jsxHeading = <h1 id="heading"> Namaste React using JSX 🚀</h1>; // using JSX
 console.log(jsxHeading);
+
+// React Functional Component
+const HeadingComponent = () => {
+    return <h1>Namaste React Functional Component</h1>
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 

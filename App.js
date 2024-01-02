7b260fc7d7c67631e88459a806fa460c4713040c
Prashant Chakraborty <prashantchakraborty@Prashants-MBP.lan>
2024-01-01 22:28:21 -0600

    Namaste React Ep 3 Part 4- Practiced Component Composition

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// React Element
-const jsxHeading = <h1 id="heading"> Namaste React using JSX 🚀</h1>; // using JSX
-console.log(jsxHeading);
+const Title = () => (
+    <h1 className="head" tabIndex="5">
+        Namaste React using JSX 🚀
+    </h1>
+)
 
-// React Functional Component
-const HeadingComponent = () => {
-    return <h1>Namaste React Functional Component</h1>
-}
-
+const HeadingComponent = () => (
+    <div id="container">
+        <Title />
+        <h1 className="heading">Namaste React Functional Component</h1>
+    </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(jsxHeading);
+root.render(<HeadingComponent />);

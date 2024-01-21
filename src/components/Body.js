2fdfed0b9de3c640f3b645975dd752f1f9bf8934
Prashant Chakraborty <prashantchakraborty@Prashants-MBP.attlocal.net>
2024-01-20 20:25:44 -0600

    Namaste React useState Practice

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,12 +2,42 @@ import RestaurantCard from "./RestaurantCard";
 import { useState } from "react";
 import resList from "../utils/mockData";
 
+function filterData(searchText, restaurants) {
+  const filterData = restaurants.filter((restaurant) => 
+    restaurant.data.name.includes(searchText));
+
+  return filterData;
+}
+
 const Body = () => {
     // State Variable- Super powerful variable
     const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+    const [searchText, setSearchText] = useState("");
 
   return (
     <div className="body">
+      <div className="search-container">
+        <input 
+          type="text" 
+          className="search-input"
+          placeholder="Search"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value)
+          }} 
+        />
+
+        <button 
+          className="search-btn"
+          onClick= {()=> {
+            const data= filterData(searchText, listOfRestaurants);
+            setListOfRestaurants(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
+
       <div className="filter">
         <button
           className="filter-btn"
